ae2017a4aa6cfaa0f6b765b010563a7bd530c223
kms278711 <35741985+kms278711@users.noreply.github.com>
2022-10-01 03:35:21 +0900

    문자열 5~8

---
+++ inflearn/palindromeByStringBuilder.java
@@ -0,0 +1,19 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class palindromeByStringBuilder {
+    public String solution(String str) {
+        String answer="NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equalsIgnoreCase(tmp)) answer="YES";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        palindromeByStringBuilder T = new palindromeByStringBuilder();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
